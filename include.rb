0f577a2b132044168074a5589c51a5bb101299c6
joshsarna <joshsarna@gmail.com>
2019-02-01 16:04:43 -0800

    require a file in another file

---
+++ include.rb
@@ -0,0 +1,15 @@
+require ('./class-variables.rb')
+
+book = Book.new("Lord of the Flies", "William Golding", 208)
+p book.title
+
+=begin
+
+0
+"The Grapes of Wrath"
+464
+"The Crying of Lot 49"
+152
+"Lord of the Flies"
+
+=end
